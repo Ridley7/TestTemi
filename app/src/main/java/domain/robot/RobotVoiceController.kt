aa733c80d867e49c86d81b487c66c44a3cb56cce
Franks <gonzalezfranks@hotmail.com>
2025-12-19 14:02:44 +0100

    add: Mas pruebas con voz, pero sigue sin funcionar

--- app/src/main/java/domain/robot/RobotVoiceController.kt
+++
@@ -1,4 +0,0 @@
-package domain.robot
-
-interface RobotVoiceController {
-}
--- app/src/main/java/presentation/ui/VoiceCommandsScreen.kt
+++ app/src/main/java/presentation/ui/VoiceCommandsScreen.kt
@@ -1,9 +1,11 @@
 package presentation.ui
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,8 +22,113 @@ import androidx.compose.ui.unit.sp
 import com.robotemi.sdk.Robot
 import com.robotemi.sdk.SttLanguage
 import com.robotemi.sdk.TtsRequest
+import com.robotemi.sdk.listeners.OnConversationStatusChangedListener
 import config.TemiConstants
 
+
+@Composable
+fun VoiceCommandsScreen(
+    robot: Robot,
+    onBack: () -> Unit
+) {
+
+    var asrState by remember { mutableStateOf(0) }
+    var lastText by remember { mutableStateOf("TEst") }
+    var backgroundColor by remember { mutableStateOf(Color.DarkGray) }
+
+    DisposableEffect(Unit) {
+
+
+        val conversationListener = object : OnConversationStatusChangedListener {
+            override fun onConversationStatusChanged(status: Int, text: String) {
+
+                when (status) {
+                    OnConversationStatusChangedListener.THINKING -> {
+                        // Frase final reconocida
+                        val command = text.lowercase()
+                        lastText = text
+
+
+                        when {
+                            command.contains("base") -> robot.goTo(TemiConstants.HOME_BASE)
+                            command.contains("puerta") -> {
+                                backgroundColor = Color(0xFF3B82F6)
+
+                                val ttsRequest = TtsRequest.create(
+                                    speech = "Voy a la puerta",
+                                    isShowOnConversationLayer = false,
+                                    language = TtsRequest.Language.ES_ES
+                                )
+
+                                robot.goTo(TemiConstants.DOOR)
+
+                                robot.speak(ttsRequest)
+
+
+                            }
+                            command.contains("para") -> robot.stopMovement()
+                        }
+                    }
+
+                    OnConversationStatusChangedListener.LISTENING -> {
+                        // Temi está escuchando
+
+                    }
+
+                    OnConversationStatusChangedListener.SPEAKING -> {
+                        // Temi está hablando
+                    }
+
+                    OnConversationStatusChangedListener.IDLE -> {
+                        // Conversación terminada
+                    }
+                }
+            }
+        }
+
+
+        robot.addOnConversationStatusChangedListener(conversationListener)
+
+        onDispose {
+            robot.removeOnConversationStatusChangedListener(conversationListener)
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .safeDrawingPadding()
+    ) {
+
+        Button(
+            onClick = { robot.wakeup() },
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text("Hablar")
+        }
+
+        Text(
+            text = lastText,
+            modifier = Modifier.align(Alignment.BottomCenter),
+            color = Color.White,
+            fontSize = 20.sp
+        )
+
+        Button(
+            onClick = onBack,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.TopStart)
+        ) {
+            Text("Volver")
+        }
+    }
+}
+
+
+
+/*
 @Composable
 fun VoiceCommandsScreen(
     robot: Robot,
@@ -118,3 +225,4 @@ fun VoiceCommandsScreen(
 
 
 }
+*/
